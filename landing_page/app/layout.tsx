9223e3ef59e264b9b57cb439f0152ec630d6d0e3
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-05-05 15:50:55 -0300

    Mudando a fonte das letras

--- landing_page/app/layout.tsx
+++ landing_page/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Luckiest_Guy } from "next/font/google";
 import "./globals.css";
+
+const luckiestGuy = Luckiest_Guy({
+  subsets: ['latin'],
+  weight: '400',
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +32,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`min-h-full flex flex-col ${luckiestGuy.className}`}>{children}</body>
     </html>
   );
 }
